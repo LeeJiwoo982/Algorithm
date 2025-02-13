138b10cfd027f6f4c7d4e8d553d58b044017199e
ijioo <easywoo980902@gmail.com>
2025-02-13 14:04:38 +0900

    add comment

--- 02_algorithm/00_List_l/2005_pascalsTri/2005.py
+++ 02_algorithm/00_List_l/2005_pascalsTri/2005.py
@@ -8,9 +8,9 @@ for tc in range(1,T+1):
     for i in range(1, N):   #첫 값은 이미 있어서 두번째 부터 값을 넣기
         row =[1]
         for j in range(1,i):    #중앙값 계산
-            row.append(tri[i-1][j-1] + tri[i-1][j])
-        row.append(1)
-        tri.append(row)
+            row.append(tri[i-1][j-1] + tri[i-1][j]) # 기억자로 더하기
+        row.append(1)   # 마지막 값 추가
+        tri.append(row) # 트라이앵글 완성
     print(f'#{tc}')
     for row in tri:
         print(" ".join(map(str, row)))
