0bf099fc90f18f5fd6a7f7546dc3cd0a7d66b1d4
ijioo <easywoo980902@gmail.com>
2025-02-20 11:00:47 +0900

    Update problems

--- 02_algorithm/03_Queue_q/0_contents_queue/bfs_practice1.py
+++ 02_algorithm/03_Queue_q/0_contents_queue/bfs_practice1.py
@@ -2,10 +2,60 @@
 # 모든 정점을 너비우선탐색하여 경로를 출력
 # 시작정점은 1
 '''
-1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7
+7 8
+4 2 1 2 1 3 5 2 4 6 5 6 6 7 3 7
 '''
 '''
 출력 결과
 1-2-3-4-5-7-6
 '''
-adj = list(map(int, input().split()))
+# arr = list(map(int, input().split()))
+# N = len(arr)
+# adj = [[] for _ in range(8)]    #그래프 경로 저장, 행은 출발노드, 열은 도착노드
+# # adj = [[], [2, 3], [4, 5], [7], [6], [6], [7], []]
+# for i in range(0, N, 2):
+#     adj[arr[i]].append(arr[i+1])
+#
+'''
+7 8
+4 2 1 2 1 3 5 2 4 6 5 6 6 7 3 7
+'''
+#선생님 코드
+def bfs(s, V):  # 시작정점 s, 마지막 정점 V
+    ''''''
+    visited = [0] * (V+1)   # visited 생성
+    q = []          # 큐 생성
+    q.append(s)     # 시작점 인큐
+    visited[s] = 1  # 시작점 방문표시
+    while q:        # 큐에 정점이 남아있으면 front != rear
+        t = q.pop(0)    # 디큐
+        print(t, end = ' ')        # 방문한 정점에서 할일
+        for w in adj_l[t]:  # 인접한 정점 중 인큐되지 않은 정점 w가 있으면
+            if visited[w]==0:
+                q.append(w)     # w인큐, 인큐되었음을 표시
+                visited[w] = visited[t] + 1
+    print(visited)
+
+V, E = map(int, input().split()) # 1번부터 V번 정점, E개의 간선
+arr = list(map(int, input().split()))
+
+# 인접리스트 -------------------------
+adj_l = [[] for _ in range(V+1)]
+for i in range(E):
+    v1, v2 = arr[i*2], arr[i*2+1]
+    adj_l[v1].append(v2)
+    adj_l[v2].append(v1)    # 방향이 없는 경우
+
+#다른경우 range(0, E*2, 2)
+#v1, v2 = arr[i], arr[i+1]
+# 여기까지 인접리스트 -----------------
+
+
+d= bfs(5, 7)
+# start = 1
+# 경로 1 2 3 4 5 7 6
+# visited = [0, 1, 2, 2, 3, 3, 4, 3]
+
+#start= 5
+# 경로 5 2 6 4 1 7 3
+# visited = [0, 3, 2, 4, 3, 1, 2, 3]
---
+++ 02_algorithm/03_Queue_q/5105-mazeDistance/t_5105.py
@@ -0,0 +1,32 @@
+def bfs(i, j, N):
+    # 준비
+    visited = [[0]*N for _ in range(N)] # visited 생성
+    q = []      # 큐생성
+    q.append([i,j])# 시작점 인큐 , 튜플로 묶기도 가능    #출발지가 두곳이나 여러곳 이라면 여기서 for문 돌리기
+    visited[i][j] = 1# 시작점 인큐 표시
+    # 탐색
+    while q:
+        ti, tj = q.pop(0)   # 디큐
+        if maze[ti][tj] == 3:   # visit(t)
+            return visited[ti][tj] - 1 - 1 # 경로의 빈칸 수, -1 추가
+        for di, dj in [[0,1],[1,0],[0,-1],[-1,0]]: # 미로내부고, 인접이고 벽이아니면,
+            wi, wj = ti+di, tj+dj
+            if 0<=wi<N and 0<=wj<N and maze[wi][wj] != 1 and visited[wi][wj] == 0:
+                q.append([wi, wj])# 인큐
+                visited[wi][wj] = visited[ti][tj] + 1   # 인큐 표시
+    return 0
+
+def find_start(N):
+    for i in range(N):
+        for j in range(N):
+            if maze[i][j] == 2: #만약 스트링으로 받았다면 '2'로 스트링형 표현**
+                return i, j
+
+N = int(input())
+maze = [list(map(int, input())) for _ in range(N)]
+
+sti, stj = find_start(N)
+
+#결과를 받기. 시작위치, i, j, 크기N
+ans = bfs(sti, stj, N)
+print(ans)
