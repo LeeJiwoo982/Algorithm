d90583bc725b69839c5276b0e318ada4e001d99e
LeeJiwoo <easywoo980902@gmail.com>
2025-03-05 15:04:39 +0900

    Add s5176 file

--- 02_algorithm/04_Tree/s5176_binarySearch/s5176.py
+++ 02_algorithm/04_Tree/s5176_binarySearch/s5176.py
@@ -3,15 +3,30 @@
 # 왼쪽, 현재, 오른쪽
 # 완전이진트리
 
+#중위순회로 넣기
+def in_order(n):
+    '''자연수를 중위순회 순서로 넣는다.'''
+    global cnt, N
+    left = 2*n  #배열 인덱스 안에 있다면
+    right = 2*n+1
 
+    if left <= N:
+        in_order(left)
 
+    tree[n] = cnt
+    cnt += 1
+
+    if right<=N:
+        in_order(right)
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
 
     tree = [0]*(N+1)
-    last = 0
+    cnt = 1
+    in_order(1)
+    # print(tree)
 
-    # print(f'#{tc} {N//2+1} {}')
+    print(f'#{tc} {tree[1]} {tree[N//2]}')
     # root 값, N//2 노드 값
